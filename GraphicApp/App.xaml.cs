4b4eab8807fd10c528f8a650c6c8ab360e105ea5
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2023-12-05 20:01:49 +0100

    Add project files.

---
+++ GraphicApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GraphicApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
